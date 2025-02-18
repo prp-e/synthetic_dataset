2fe6d3bc793ed5447b757461fa04e07eefc8453d
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-02-18 19:19:32 +0330

    hope this one works well

--- main.py
+++ main.py
@@ -3,7 +3,32 @@ from openai import OpenAI
 client = OpenAI(base_url = "https://text.pollinations.ai/openai", api_key="sk-0000000000000000000000000")
 
 def questions_creation(topic, language, qty):
-    pass
+    questions = []
+
+    for q in range(qty):
+        try:
+            question = client.chat.completions.create(
+        model = "openai",
+        messages = [
+            {
+                "role" : "system",
+                "content" : "You are a question generator. The user gives you a topic and a language, you must generate a question in that topic and language. Do not use markdown."
+            },
+            {
+                "role" : "user",
+                "content" : f"topic: {topic}\nlanguage:{language}"
+            }
+        ],
+        temperature = 0.5 
+    )
+            question = question.choices[0].message.content
+            questions.append(question)
+        except:
+            print("An error occured.")
+        
+    return questions
+
+
 
 
 if __name__ == "__main__":
