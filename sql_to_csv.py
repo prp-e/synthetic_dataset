d693672537f315003068fed24e4f8dd70abe6d80
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-03-06 10:43:45 +0330

    another correction

--- sql_to_csv.py
+++ sql_to_csv.py
@@ -1,5 +1,4 @@
 import sqlite3
-import pandas as pd
 
 conn = sqlite3.connect("dataset.sqlite")
 cursor = conn.cursor()
@@ -9,6 +8,6 @@ rows = cursor.fetchall()
 
 data = [item for row in rows for item in row]
 
-print(data)
+
 
 conn.close()
