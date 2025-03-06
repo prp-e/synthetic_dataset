5ff528c419faaf3d2a002e762cbe6412ed3077ef
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-03-06 10:42:53 +0330

    working on it

--- sql_to_csv.py
+++ sql_to_csv.py
@@ -4,3 +4,11 @@ import pandas as pd
 conn = sqlite3.connect("dataset.sqlite")
 cursor = conn.cursor()
 
+cursor.execute("SELECT question, answer FROM data")
+rows = cursor.fetchall()
+
+data = [item for row in rows for item in row]
+
+print(data)
+
+conn.close()
